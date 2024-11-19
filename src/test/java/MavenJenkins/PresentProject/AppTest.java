b564a5b998c60ab0109e490a00825f01b48581bf
smv-manovihar <sirimallamanovihar@gmail.com>
2024-11-19 23:27:12 +0530

    First Commit

---
+++ src/test/java/MavenJenkins/PresentProject/AppTest.java
@@ -0,0 +1,20 @@
+package MavenJenkins.PresentProject;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
